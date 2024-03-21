5e9968dedea5d64eb76baf39c7b10667e5bb043c
Novo1999 <neptune.navo@gmail.com>
2024-03-21 14:16:29 +0600

    remove: unnecessary codes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,9 @@
 import { motion } from 'framer-motion'
-import { useEffect } from 'react'
-import { toast } from 'react-toastify'
 import ProductContainer from '../components/Product/ProductContainer.jsx'
 import ProductList from '../components/Product/ProductList.jsx'
 import Carousel from '../components/ui/Carousel.jsx'
-import useAuthContext from '../hooks/useAuthContext.js'
 
 const Home = () => {
-  const { user: { accessToken, username } = {} } = useAuthContext()
-  // greet the user
-  useEffect(() => {
-    if (accessToken) {
-      toast.success(`Welcome ${username}`, { autoClose: 1000 })
-    }
-  })
-
   return (
     <motion.div>
       <Carousel />
