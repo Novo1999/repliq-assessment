fe7c6290e7348083bf2cbc708078dded79106bba
Novo1999 <neptune.navo@gmail.com>
2024-03-21 14:55:17 +0600

    removed: useless motion from home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,15 @@
-import { motion } from 'framer-motion'
 import ProductContainer from '../components/Product/ProductContainer.jsx'
 import ProductList from '../components/Product/ProductList.jsx'
 import Carousel from '../components/ui/Carousel.jsx'
 
 const Home = () => {
   return (
-    <motion.div>
+    <div>
       <Carousel />
       <ProductContainer>
         <ProductList />
       </ProductContainer>
-    </motion.div>
+    </div>
   )
 }
 export default Home
